f66a74bffb0fe619f67df1b15e07b77b4060c983
Alex Fiannaca <fiannaca@cs.washington.edu>
2015-04-03 11:54:38 -0700

    Basic example is working

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,30 @@
 (function (document) {
   'use strict';
 
+  var fb = new Firebase("https://hcrkubi.firebaseio.com/");
+  var commands = fb.child("commands");
+  var log = fb.child("wozlog");
+
+  //log.push().set({
+  //  time: Firebase.ServerValue.TIMESTAMP,
+  //  event: "CONNECT",
+  //  details: "Web app connected to Firebase"
+  //});
+
+
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
-  app.appName = 'Yo, Polymer App!';
+  app.appName = 'Kubi Tutor - Wizard of Oz Controller App';
+  app.menuOption = 0;
+
+  app.sampleBtnClick = function() {
+    commands.push().set({
+      time: Firebase.ServerValue.TIMESTAMP,
+      command: "SAMPLE_COMMAND_1"
+    });
+  };
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
