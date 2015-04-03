fe287a589107885e979d1e78d4a6eb9e14df61d1
Alex Fiannaca <fiannaca@cs.washington.edu>
2015-04-03 14:23:30 -0700

    Ready to be handed off to Kim

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,24 +1,55 @@
 (function (document) {
   'use strict';
 
-  var fb = new Firebase("https://hcrkubi.firebaseio.com/");
-  var commands = fb.child("commands");
-  var log = fb.child("wozlog");
-
-  //log.push().set({
-  //  time: Firebase.ServerValue.TIMESTAMP,
-  //  event: "CONNECT",
-  //  details: "Web app connected to Firebase"
-  //});
-
-
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
+  // Add some properties to our auto-binding template:
+  //  Any field added to the `app` object can be binded to using {{}} notation in index.html
   var app = document.querySelector('#app');
   app.appName = 'Kubi Tutor - Wizard of Oz Controller App';
   app.menuOption = 0;
 
+  app.email = null;
+  app.password = null;
+
+  // Connect to the firebase and setup the login function
+  var fb = new Firebase("https://hcrkubi.firebaseio.com/");
+  app.submitLogin = function() {
+    console.log("Logging in...");
+
+    fb.authWithPassword({
+      email    : app.email,
+      password : app.password
+    }, function (error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        document.querySelector('#loginDialog').opened = false;
+        app.authenticated = true;
+        console.log("Authenticated successfully with payload:", authData);
+      }
+    })
+  };
+
+  // Allow the user to logout if need be
+  app.logout = function() {
+    fb.unauth();
+    app.authenticated = false;
+    document.querySelector('#loginDialog').opened = true;
+  };
+
+  // For initialization, lets check if they are already logged in, if not, disable the buttons
+  if(fb.getAuth()) {
+    app.authenticated = true;
+  } else {
+    app.authenticated = false;
+  }
+
+  // Use this ref for adding commands to the Firebase
+  var commands = fb.child("commands");
+
+  // Use this ref for adding logging data to the Firebase
+  var log = fb.child("wozlog");
+
+  // These are examples of how to connect buttons to sending commands to the Firebase
   app.sampleBtnClick = function() {
     commands.push().set({
       time: Firebase.ServerValue.TIMESTAMP,
@@ -26,10 +57,35 @@
     });
   };
 
+  app.sampleBtnClick2 = function() {
+    commands.push().set({
+      time: Firebase.ServerValue.TIMESTAMP,
+      command: "SAMPLE_COMMAND_2"
+    });
+  };
+
+  app.sampleBtnClick3 = function() {
+    commands.push().set({
+      time: Firebase.ServerValue.TIMESTAMP,
+      command: "SAMPLE_COMMAND_3"
+    });
+  };
+
+  app.sampleBtnClick4 = function() {
+    commands.push().set({
+      time: Firebase.ServerValue.TIMESTAMP,
+      command: "SAMPLE_COMMAND_4"
+    });
+  };
+
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
   app.addEventListener('template-bound', function() {
     console.log('Our app is ready to rock!');
+
+    if(!app.authenticated) {
+      document.querySelector('#loginDialog').opened = true;
+    }
   });
 
 // wrap document so it plays nice with other libraries
